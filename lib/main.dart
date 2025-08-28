c22b8f963105b8e3721f39a494de8750d9daf04a
DianRestu <DianRestu13>
2025-08-28 12:10:53 +0700

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'text_widget.dart';
+import 'image_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+      debugShowCheckedModeBanner: false,
+      home: Center(child: MyTextWidget(),), 
+    
+    // return MaterialApp(
+    //  debugShowCheckedModeBanner: false,
+    //    home: Scaffold(body: Center(child: MyImageWidget(),),)
+     // title: 'Flutter Demo',
+      //theme: ThemeData(
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
@@ -28,9 +36,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+     // ),
+     // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -104,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const MyImageWidget(),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
---
+++ lib/text_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart' ;
+
+class MyTextWidget extends StatelessWidget {
+  const MyTextWidget ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Hello, World!",
+      style: TextStyle(fontSize: 24),
+      textAlign: TextAlign.center
+    );
+  }
+}
